2afdb17bef35af64c51b989cc43125f5be4220e5
ArtemiiKokodeev <artemkokodeev@gmail.com>
2024-01-15 11:25:06 +0400

    16. Installed older version of redux-thunk npm package to fix start project error

--- src/services/store.js
+++ src/services/store.js
@@ -1,13 +1,13 @@
 import {applyMiddleware, createStore} from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from "../services/reducers/index";
-import { thunk } from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
 
 export const configureStore = (initialState) => {
   const store = createStore(
     reducer,
     initialState,
-    composeWithDevTools(applyMiddleware(thunk))
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
   );
 
   return store;
